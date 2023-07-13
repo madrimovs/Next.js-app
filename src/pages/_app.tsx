95dcfdcba704fea19fb50647576c83ec9b029a3e
madrimovs <sardormadrimov516@gmail.com>
2023-07-13 22:24:23 +0500

    nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,10 @@ import { Roboto } from "next/font/google";
 import Head from "next/head";
 import "../styles/globals.css";
 
+import "nprogress/nprogress.css";
+import NProgress from "nprogress";
+import { useEffect } from "react";
+
 const roboto = Roboto({
    weight: ["300", "400", "500", "700"],
    style: ["normal", "italic"],
@@ -27,7 +31,24 @@ export default function App(props: MyAppProps) {
       Component,
       emotionCache = clientSideEmotionCache,
       pageProps,
+      router,
    } = props;
+
+   useEffect(() => {
+      const handleRouteStart = () => NProgress.start();
+      const handleRouteDone = () => NProgress.done();
+
+      router.events.on("routeChangeStart", handleRouteStart);
+      router.events.on("routeChangeComplete", handleRouteDone);
+      router.events.on("routeChangeError", handleRouteDone);
+
+      return () => {
+         // Make sure to remove the event handler on unmount!
+         router.events.off("routeChangeStart", handleRouteStart);
+         router.events.off("routeChangeComplete", handleRouteDone);
+         router.events.off("routeChangeError", handleRouteDone);
+      };
+   }, []);
 
    return (
       <CacheProvider value={emotionCache}>
